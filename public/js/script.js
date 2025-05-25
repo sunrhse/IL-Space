22d70ab6bdb53c51d7cfffeeedfa6bbc9c75e843
Isaura de Lourdes <isauralourdes23@gmail.com>
2025-05-25 14:07:35 -0300

    feat - Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,30 @@
-function toggleBox()
-{
+function toggleBox() {
   const box = document.getElementById('cvBox');
   box.classList.toggle('expanded');
   const toggleText = box.querySelector('.box-toggle');
   toggleText.textContent = box.classList.contains('expanded') ? '▲ Ver menos' : '▼ Ver mais';
 }
+
+function openModal(id) {
+  document.getElementById(id).style.display = 'flex';
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+window.addEventListener('click', function (event) {
+  document.querySelectorAll('.modal').forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
+
+document.querySelectorAll('.modal-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    const modal = this.closest('.modal');
+    modal.style.display = 'none';
+  });
+});
+
